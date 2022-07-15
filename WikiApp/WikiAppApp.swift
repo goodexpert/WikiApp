4afdf541a5c574dda3e7d5278e2e5729d1612d9f
Steve Park <seongwuk.park@gmail.com>
2022-07-15 13:02:53 +1200

    Initial Commit

---
+++ WikiApp/WikiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiAppApp.swift
+//  WikiApp
+//
+//  Created by Seongwuk Park on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WikiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
